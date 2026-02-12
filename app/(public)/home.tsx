21d4f0219c7ab98bc2de091b6d15e27fc35535e3
kendy code <kenphiliboy@gmail.com>
2026-02-12 21:36:25 +0000

    update home screen and _layout adding quiz list scrolling and quiz preview

--- app/(public)/home.tsx
+++ app/(public)/home.tsx
@@ -1,12 +1,23 @@
 import Screen from '@/components/Screen';
 import HomeHeader from '@/components/HomeHeader';
+import FaeturedCarousel from '@/components/FeaturedCarousel';
+import { useEffect, useState } from 'react';
+import { QuizPreview } from '@/types/quiz';
+import { nestApi } from '@/api/nest';
 
 export default function HomeScreen() {
+const [quizzes, setQuizzes] = useState<QuizPreview[]>([]);
+
+  useEffect(() => {
+    nestApi.get<QuizPreview[]>('/quiz').then(res => {
+      setQuizzes(res.data);
+    });
+  }, []);
   return (
     <Screen>
       <HomeHeader />
 
-      {/* next: FeaturedCarousel */}
+      <FaeturedCarousel quizzes={quizzes} />
       {/* next: CategoryTabs */}
       {/* next: Quiz list */}
     </Screen>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,8 +8,7 @@ export default function RootLayout() {
                 screenOptions={{
                     headerShown: false,
                 }}
-            />;
+            />
         </ThemeProvider>
     )
-}
-
+};
