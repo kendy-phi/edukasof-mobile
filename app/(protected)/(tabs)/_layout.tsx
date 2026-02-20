879cff73858d5bed724aa31d62b5ea4cb9a8bd32
ashley phili <kenphiliboy@gmail.com>
2026-02-20 16:58:21 +0000

    last update to push on github

---
+++ app/(protected)/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+        backBehavior="history"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#2563eb',
+        tabBarInactiveTintColor: '#6b7280',
+        tabBarStyle: {
+          height: 100,
+          paddingBottom: 50
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="quizzes"
+        options={{
+          title: 'Quiz',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profil',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(protected)/(tabs)/quizzes.tsx
@@ -0,0 +1,54 @@
+import { nestApi } from '@/api/nest';
+import { QuizCard } from '@/components/QuizCard';
+import Screen from '@/components/Screen';
+import { Quiz } from '@/types/quiz';
+import { getAllQuizProgress } from '@/utils/quizProgress';
+import { router } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList } from 'react-native';
+export default function QuizzesScreen() {
+  const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+  const [quizProgressMap, setQuizProgressMap] = useState<Record<string, number>>({});
+
+
+  useEffect(() => {
+    nestApi.get<Quiz[]>('/quiz').then(res => {
+      // console.log(res.data);
+
+      setQuizzes(res.data.filter(q => q.isPublished));
+    });
+  }, []);
+
+  useEffect(() => {
+    const loadProgress = async () => {
+      const saved = await getAllQuizProgress();
+
+      if (!saved) {
+        setQuizProgressMap({});
+        return;
+      }
+      // console.log(saved);       
+      setQuizProgressMap(saved);
+    };
+
+    loadProgress();
+  }, []);
+
+
+  return (
+    <Screen>
+      <FlatList
+        data={quizzes}
+        contentContainerStyle={{ gap: 12 }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <QuizCard
+            quiz={item}
+            progressPercentage={quizProgressMap[item.id] ?? 0}
+            onPress={() => router.push(`/quiz/${item.id}`)}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
