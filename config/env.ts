8ed6e38d69c09c381cd4da9da742c2aab2d6bd20
ashley phili <kenphiliboy@gmail.com>
2026-03-02 00:40:50 +0000

    last change to push on the server to deploy

--- config/env.ts
+++ config/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
   LARAVEL_API: 'https://laravel.ngrok.io/api',
-  NEST_API: 'https://ab58-200-113-217-48.ngrok-free.app/api/v1' //'http://10.204.175.24:3250/api/v1' //
+  NEST_API: 'https://quiz-api.edukasof.com/api/v1' //'http://10.204.175.24:3250/api/v1' //
 };
